f44ca9be7eaa47b5cdd9847ae73e134e12fbcb7a
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-28 13:48:41 +0330

    #2.3.2 angular syntax and basics

--- expressapp/client/main.js
+++ expressapp/client/main.js
@@ -1,5 +1,25 @@
 let todoApp = angular.module('todo-app', [])
 
 todoApp.controller('todoCtrl', function ($scope) {
+  // wtf is $scope? make sure to understand it properly
+
   $scope.test = 'hello angular'
+
+  // testing ng-model
+  $scope.formData = 'yo'
+
+  // functions and clicks
+  $scope.aFunc = function (param1, param2) {
+    alert('i was called')
+  }
+  $scope.giveMeSomething = function () {
+    return 'booo'
+  }
+
+  // iterations
+  $scope.list = [
+    {name: 'kian', id: 92521052 },
+    {name: 'asqar', id: 92521044 },
+    {name: 'qolam', id: 92521034 }
+  ]
 })
