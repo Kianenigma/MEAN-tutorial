e178be6cc63614ba2a2a27639aa07cc85db21d76
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-28 12:26:12 +0330

    #2.2.1 mongoose

--- expressapp/app.js
+++ expressapp/app.js
@@ -2,12 +2,35 @@ let express = require('express')
 let bodyParser = require('body-parser')
 let app = express()
 
+// ------ global middlewares
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
 
 app.use(require('./logger.middleware'))
+
+// ------- Database Config
+let mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/mean')
+let db = mongoose.connection
+db.on('error', (err) => { console.log('error with db', err) })
+db.once('open', function () {
+  console.log('DB Connected')
+})
+
+const Schema = mongoose.Schema
+
+const TaskSchema = new Schema({
+  title: { type: string, require: true},
+  done: {type: Boolean}
+})
+
+let TaskModel = mongoose.model('TaskModel', TaskSchema)
+
+// ---------
+
 app.get('/', (req, resp) => {
   // things we have in request object
   console.log(`base url ${req.baseUrl}`)
@@ -16,6 +39,7 @@ app.get('/', (req, resp) => {
   console.log(`cookies ${req.cookies}`)
   console.log(`method ${req.method}`)
   console.log(`path ${req.path}`)
+  console.log(`params ${JSON.stringify(req.params)}`)
   console.log(`query ${JSON.stringify(req.query)}`)
 
   // test :
@@ -40,7 +64,7 @@ app.post('/yo', (req, resp) => {
 // or a 404 not found page
 app.use((req, resp) => {
   // any Idea how this works?
-  // Know the locaion header? 
+  // Know the locaion header?
   resp.redirect('/')
 })
 
